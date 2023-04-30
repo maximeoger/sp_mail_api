58f9770e871d2276e745016c4c2a44305efee095
maximeoger <maximeoger93@proton.me>
2023-04-30 13:12:54 +0200

    [refactoring] Removed excluded_keyword and included_keywords attributes in GamesWorkshop class since we only search for what we are looking for in emails for this supplier

--- src/domains/GamesWorkshop/GamesWorkshop.ts
+++ src/domains/GamesWorkshop/GamesWorkshop.ts
@@ -7,36 +7,6 @@ export default class GW extends Supplier {
 
   constructor(data: SupplierData) {
     super(data);
-    // TODO: Faire venir ces données d'un backend externe
-    this.excluded_keywords = [
-      'Mise à jour',
-      'service commercial',
-      'Informations relatives',
-      'departement commercial',
-      'Formulaire de Retour',
-      'déménage',
-      'votre commande',
-      'jeu organisé',
-      'rappel du produit',
-      'produit'
-    ]
-    this.included_keywords = [
-      'warhammer',
-      'warhammer 40.000',
-      '40,000',
-      '40000',
-      'warhammer Age of Sigmar',
-      'age of sigmar',
-      'tomes de battaille',
-      'warcry',
-      'necromunda',
-      'battleforces',
-      'codex',
-      'citadel',
-      'hérésie',
-      'horus',
-      'blood bowl'
-    ]
   }
 
   private async retreiveDownloadLinkFromParsedEmail(html: string): Promise<string> {
